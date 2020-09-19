94ee9ed140892c30ad1f6fd3f3e8a13de48bfe0f
Pavel Stepanets <pahhan.ne@gmail.com>
2020-09-19 13:33:23 +0700

    add game

---
+++ src/Game.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+
+namespace Stepanets\SeaBattle;
+
+
+final class Game
+{
+
+}
---
+++ tests/GameTest.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+
+namespace Stepanets\SeaBattle\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+final class GameTest extends TestCase
+{
+
+}
